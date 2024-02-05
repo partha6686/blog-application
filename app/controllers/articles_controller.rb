5e4767624f1804007e153e66d00e83848ff1e690
Partha <parthasarathi6686@gmail.com>
2024-02-06 01:23:14 +0530

    add edit funtionality and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,30 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    def new
+        @article = Article.new
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :desc))
+        # render plain: @article.inspect
+        if @article.save
+            flash[:notice] = "Article created successfully."
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :desc))
+            flash[:notice] = "Article updated successfully."
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+    
 end
