b1cee94a32afd7316b3c09ca5f8b2db822dc20eb
Partha <parthasarathi6686@gmail.com>
2024-02-07 02:09:33 +0530

    add one-many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :articles 
     validates :username, presence: true, length: {minimum:3, maximum:25}, uniqueness: { case_sensitive: false }
     validates :email, presence: true, length: {minimum:3, maximum:105}, uniqueness: { case_sensitive: false },format: { with: /[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_06_193507) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_06_201530) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
