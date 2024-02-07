e2c71e15ce3472dcd987e7c5ccc451b425e89de1
Partha <parthasarathi6686@gmail.com>
2024-02-08 01:11:43 +0530

    add user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def gravatar_for(email, options= {size: 80})
+        hash = Digest::MD5.hexdigest(email)
+        size=options[:size]
+        url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(url, alt: 'user-avatar')
+    end
 end
